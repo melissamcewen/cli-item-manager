ebd2453460b5b440a86acf48d73fe826d8c4d296
Glitch (regal-stream) <none>
2017-11-11 02:45:37 +0000

    🎪⏳ Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# cli-item-manager
-lowdb and commander.js menu management app using only the command line
+CLI Item Manager
+=========================
+
+A very simple example of using the node command line to manage a lowdb database.
+
+
+Credits
+------------
+
+https://scotch.io/tutorials/build-an-interactive-command-line-application-with-nodejs
+https://github.com/typicode/lowdb
+https://www.sitepoint.com/javascript-command-line-interface-cli-node-js/
+
+
+scratchpad: http://jsbin.com/jecesaj/edit?js,console
---
+++ item.js
@@ -0,0 +1,30 @@
+const program = require('commander');
+// Require logic.js file and extract controller functions using JS destructuring assignment
+const { addItem, getItem, getItems } = require('./logic');
+
+program
+  .version('0.0.1')
+  .description('Item management system');
+
+program
+  .command('addItem <title> <category> <description>')
+  .alias('a')
+  .description('Add an item')
+  .action((title, category, description) => {
+    addItem(title, category, description);
+  });
+
+program
+  .command('getItem <title>')
+  .alias('r')
+  .description('Get item')
+  .action(title => getItem(title));
+
+program
+  .command('listItems')
+  .alias('l')
+  .description('Get items')
+  .action(getItems);
+
+
+program.parse(process.argv);
